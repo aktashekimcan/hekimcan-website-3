3dfddad1f5adccd0d0e368772e47cafbc68b7b71
aktashekimcan <108878518+aktashekimcan@users.noreply.github.com>
2024-03-04 10:38:08 +0300

    Updated _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,26 @@
 // pages/_app.js
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import '../styles/globals.css';
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Preloader from "./components/LoadingAnimation"; // Import the Preloader component
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Set a timeout to simulate the loading time
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 8000); // Adjust the time as needed
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
+      {loading && <Preloader />} {/* Render Preloader only while loading */}
       <Navbar />
       <Component {...pageProps} />
       <Footer />
