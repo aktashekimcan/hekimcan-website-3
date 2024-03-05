e2b2a908e4907111d430c0b1627c6957f01ad41e
aktashekim <108878518+aktashekimcan@users.noreply.github.com>
2024-03-05 16:55:08 +0300

    Updated about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -72,7 +72,7 @@ const About = () => {
           <h1>Hekim Aktaş</h1>
           <p>
             2017 yılından bu yana Full Stack Web Geliştirme alanında çalışan bir
-            software developer'ım.
+            Software Developer&apos;ım.
           </p>
         </Card>
         <Card>
