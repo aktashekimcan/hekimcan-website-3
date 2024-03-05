f4166ec63775224bd5cac837455d6e2d12aeabcf
aktashekim <108878518+aktashekimcan@users.noreply.github.com>
2024-03-05 11:19:19 +0300

    Updated _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,31 @@
-// pages/_app.js
+// pages/_app.tsx
+import { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Loader from "./components/LoadingAnimation"; // Yükleyici bileşeni
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // 3 saniye sonra yükleyiciyi kapat
+    const timer = setTimeout(() => setLoading(false), 8000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
-      <Navbar />
-      <Component {...pageProps} />
-      <Footer />
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Navbar />
+          <Component {...pageProps} />
+          <Footer />
+        </>
+      )}
     </>
   );
 }
