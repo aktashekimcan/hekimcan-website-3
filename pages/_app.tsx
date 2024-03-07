1197e9d9ed8ba01aad9ccbc7ed5e18e0ce5b98e0
aktashekim <108878518+aktashekimcan@users.noreply.github.com>
2024-03-07 10:21:33 +0300

    Updated _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,20 @@
 // pages/_app.tsx
 import { useState, useEffect } from "react";
-import Head from 'next/head';
+import Head from "next/head";
 import { useRouter } from "next/router";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Loader from "./components/LoadingAnimation"; // Yükleyici bileşeni
 import "../styles/globals.css";
+import styled from "styled-components";
+const MainContent = styled.main`
+  margin-left: 300px;
+
+  @media (max-width: 768px) {
+    margin-left: 0;
+  }
+`;
 
 function MyApp({ Component, pageProps }) {
   const [loading, setLoading] = useState(true);
@@ -25,33 +33,63 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-       
-        <meta name="description" content="Hekim Aktaş'ın kişisel software developer portfolyosu." />
-        <meta name="keywords" content="Hekim Aktaş, Software Developer, Web Developer, Portfolio" />
+        <title>Hekim Aktaş - Software Developer Portfolio</title>
+        <meta
+          name="description"
+          content="Hekim Aktaş'ın kişisel software developer portfolyosu."
+        />
+        <meta
+          name="keywords"
+          content="Hekim Aktaş, Software Developer, Web Developer, Portfolio"
+        />
         <meta name="author" content="Hekim Aktaş" />
 
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="Hekim Aktaş - Software Developer Portfolio" />
-        <meta property="og:description" content="Hekim Aktaş'ın kişisel software developer portfolyosu." />
-        <meta property="og:image" content="https://media.licdn.com/dms/image/D5603AQGaUrkZgKeBcA/profile-displayphoto-shrink_800_800/0/1697036903040?e=1715212800&v=beta&t=SUSU1ncYMe8tGXcmQfMPbXKgWCuy62vSAke8gO-pgvo" />
+        <meta
+          property="og:title"
+          content="Hekim Aktaş - Software Developer Portfolio"
+        />
+        <meta
+          property="og:description"
+          content="Hekim Aktaş'ın kişisel software developer portfolyosu."
+        />
+        <meta
+          property="og:image"
+          content="https://media.licdn.com/dms/image/D5603AQGaUrkZgKeBcA/profile-displayphoto-shrink_800_800/0/1697036903040?e=1715212800&v=beta&t=SUSU1ncYMe8tGXcmQfMPbXKgWCuy62vSAke8gO-pgvo"
+        />
         <meta property="og:url" content="https://www.hekimaktas.com" />
 
-        <meta name="twitter:card" content="https://media.licdn.com/dms/image/D5603AQGaUrkZgKeBcA/profile-displayphoto-shrink_800_800/0/1697036903040?e=1715212800&v=beta&t=SUSU1ncYMe8tGXcmQfMPbXKgWCuy62vSAke8gO-pgvo" />
+        <meta
+          name="twitter:card"
+          content="https://media.licdn.com/dms/image/D5603AQGaUrkZgKeBcA/profile-displayphoto-shrink_800_800/0/1697036903040?e=1715212800&v=beta&t=SUSU1ncYMe8tGXcmQfMPbXKgWCuy62vSAke8gO-pgvo"
+        />
         <meta name="twitter:creator" content="@hekimmcann" />
-        <meta name="twitter:title" content="Hekim Aktaş - Software Developer Portfolio" />
-        <meta name="twitter:description" content="Hekim Aktaş'ın kişisel software developer portfolyosu." />
-        <meta name="twitter:image" content="https://media.licdn.com/dms/image/D5603AQGaUrkZgKeBcA/profile-displayphoto-shrink_800_800/0/1697036903040?e=1715212800&v=beta&t=SUSU1ncYMe8tGXcmQfMPbXKgWCuy62vSAke8gO-pgvo" />
+        <meta
+          name="twitter:title"
+          content="Hekim Aktaş - Software Developer Portfolio"
+        />
+        <meta
+          name="twitter:description"
+          content="Hekim Aktaş'ın kişisel software developer portfolyosu."
+        />
+        <meta
+          name="twitter:image"
+          content="https://media.licdn.com/dms/image/D5603AQGaUrkZgKeBcA/profile-displayphoto-shrink_800_800/0/1697036903040?e=1715212800&v=beta&t=SUSU1ncYMe8tGXcmQfMPbXKgWCuy62vSAke8gO-pgvo"
+        />
 
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta charSet="UTF-8" />
+        <meta charSet="UTF-8" />
       </Head>
+
       {loading ? (
         <Loader />
       ) : (
         <>
           <Navbar />
-          <Component {...pageProps} />
-          <Footer />
+          <MainContent>
+            <Component {...pageProps} />
+            <Footer />
+          </MainContent>
         </>
       )}
     </>
