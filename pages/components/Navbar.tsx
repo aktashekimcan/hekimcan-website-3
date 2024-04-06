06e8c48aa1ee724954e63731c6eda5302d6ffd4f
aktashekim <108878518+aktashekimcan@users.noreply.github.com>
2024-04-06 11:50:01 +0300

    Updated Navbar.tsx

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -1,23 +1,119 @@
 import React, { useState } from "react";
 import { Link as ScrollLink } from "react-scroll";
-import { useRouter } from "next/router";
-import {
-  Navbar,
-  Nav,
-  Container,
-  NavbarBrand,
-  NavbarToggle,
-  NavbarCollapse,
-} from "react-bootstrap";
+import styled from "styled-components";
 
+// Styled components
+const Nav = styled.nav`
+  background-color: rgba(255, 255, 255, 0.3);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+  position: fixed;
+  width: 100%;
+  top: 15px;
+  z-index: 10;
+  border-radius: 10px;
+  backdrop-filter: blur(10px);
+  @media (min-width: 769px) {
+    width: auto;
+    max-width: 769px;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  @media (max-width: 768px) {
+    top: 0;
+  }
+`;
+
+const NavContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`;
+
+const Logo = styled.a`
+  font-size: 2rem;
+  color: white;
+  text-decoration: none;
+`;
+
+const Menu = styled.ul`
+  list-style: none;
+  display: flex;
+  gap: 1rem;
+  flex-wrap: wrap;
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const MenuItem = styled(ScrollLink)`
+  color: rgba(0, 0, 0, 1);
+  text-decoration: none;
+  cursor: pointer;
+  padding: 0.5rem;
+  border-radius: 5px;
+  transition: background-color 0.2s;
+  &:hover,
+  &.active {
+    background-color: rgba(0, 123, 255, 0.2);
+  }
+`;
+
+const Hamburger = styled.button`
+  background: none;
+  border: none;
+  color: white;
+  cursor: pointer;
+  @media (min-width: 769px) {
+    display: none;
+  }
+`;
+
+const HamburgerIcon = styled.img.attrs({
+  src: "https://cdn-icons-png.flaticon.com/128/12314/12314153.png",
+  alt: "Menu",
+})`
+  width: 30px;
+  height: 30px;
+`;
+
+const CloseIcon = styled.img.attrs({
+  src: "https://cdn-icons-png.flaticon.com/128/15058/15058877.png",
+  alt: "Close",
+})`
+  width: 30px;
+  height: 30px;
+  cursor: pointer;
+  position: absolute; // Positioning the close icon absolutely to ensure it's clickable and visible
+  top: 20px; // Distance from the top of the viewport
+  right: 20px; // Distance from the right of the viewport
+`;
+
+const MobileMenu = styled.div`
+  display: none;
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(255, 255, 255, 0.5);
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  &.active {
+    display: flex;
+  }
+`;
+
+// Header component
 const Header = () => {
-  const router = useRouter();
-  // `expanded` state'ini kontrol etmek için boolean tipinde bir değer kullanılıyor.
-  const [expanded, setExpanded] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   const menuItems = [
     { href: "hero", text: "Ana Sayfa" },
-    { href: "featured-projects", text: "Öne Çıkan Projelerim" },
     { href: "about", text: "Hakkımda" },
     { href: "education", text: "Eğitim" },
     { href: "skills", text: "Diller ve Araçlar" },
@@ -25,43 +121,51 @@ const Header = () => {
     { href: "contact", text: "İletişim" },
   ];
 
+  const toggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen);
+
   return (
-    <Navbar
-      expanded={expanded}
-      expand="lg"
-      bg="dark"
-      variant="dark"
-      fixed="top"
-    >
-      <Container fluid>
-        <NavbarBrand href="#">Hekim Aktaş</NavbarBrand>
-        <NavbarToggle
-          aria-controls="responsive-navbar-nav"
-          onClick={() => setExpanded(!expanded)} // Burada `expanded` değerinin tersini kullanarak doğru türde bir değer sağlıyoruz.
-        />
-        <NavbarCollapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            {menuItems.map((item, index) => (
-              <Nav.Item key={index}>
-                <ScrollLink
-                  activeClass="active"
-                  to={item.href}
-                  spy={true}
-                  smooth={true}
-                  offset={-70}
-                  duration={500}
-                  className="nav-link"
-                  href={"#" + item.href}
-                  onClick={() => setExpanded(false)} // Menü öğesi tıklandığında navbar'ı kapat
-                >
-                  {item.text}
-                </ScrollLink>
-              </Nav.Item>
-            ))}
-          </Nav>
-        </NavbarCollapse>
-      </Container>
-    </Navbar>
+    <Nav>
+      <NavContainer>
+        <Logo href="#">Hekim Aktaş</Logo>
+        <Menu>
+          {menuItems.map((item, index) => (
+            <li key={index}>
+              <MenuItem
+                activeClass="active"
+                to={item.href}
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                href={"#" + item.href}
+              >
+                {item.text}
+              </MenuItem>
+            </li>
+          ))}
+        </Menu>
+        <Hamburger onClick={toggleMobileMenu}>
+          {isMobileMenuOpen ? null : <HamburgerIcon />}
+        </Hamburger>
+        <MobileMenu className={isMobileMenuOpen ? "active" : ""}>
+          <CloseIcon onClick={toggleMobileMenu} />
+          {menuItems.map((item, index) => (
+            <MenuItem
+              key={index}
+              activeClass="active"
+              to={item.href}
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              onClick={toggleMobileMenu}
+            >
+              {item.text}
+            </MenuItem>
+          ))}
+        </MobileMenu>
+      </NavContainer>
+    </Nav>
   );
 };
 
